590200d71b0f9d15915d7d341552e330e3bc2913
Malhar Thite <malhar.thite@virtualinstruments.com>
2020-06-09 22:17:39 -0700

    handlers configured for Microservices

--- Projects/MicroservicesProject/main.go
+++ Projects/MicroservicesProject/main.go
@@ -1,28 +1,35 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"github.com/thitemalhar/GoLang/Projects/MicroservicesProject/handlers"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		log.Println("Hello world!")
-		d, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			http.Error(rw, "Oops", http.StatusBadRequest)
-			return
-		}
+	//http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+	//	log.Println("Hello world!")
+	//	d, err := ioutil.ReadAll(r.Body)
+	//	if err != nil {
+	//		http.Error(rw, "Oops", http.StatusBadRequest)
+	//		return
+	//	}
+	//
+	//	fmt.Fprintf(rw, "Hello %s\n", d)
+	//})
+	//
+	//http.HandleFunc("/goodbye", func(rw http.ResponseWriter, r *http.Request) {
+	//	log.Println("Goodbye world!")
+	//})
+	l := log.New(os.Stdout, "products-api ", log.LstdFlags)
+	hh := handlers.NewHello(l)
+	gb := handlers.NewGoodbye(l)
 
-		fmt.Fprintf(rw, "Hello %s\n", d)
-	})
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+	sm.Handle("/goodbye", gb)
 
-	http.HandleFunc("/goodbye", func(rw http.ResponseWriter, r *http.Request) {
-		log.Println("Goodbye world!")
-	})
-
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":9090", sm)
 }
